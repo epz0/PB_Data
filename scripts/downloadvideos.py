15f800d4a50a5a71a20a33261571700bedbeb6f3
Esdras Paravizo <ep650@cam.ac.uk>
2023-05-24 18:38:10 +0100

    split functions into files

---
+++ scripts/downloadvideos.py
@@ -0,0 +1,9 @@
+# accessing script with functions
+from gallerydata import *
+
+# download entries' video and thumbnail
+# originalBudget is the level budget
+# levelName is the name of the level on the API
+# these info can be retrieved from \support\LevelsInfo-master.xlsx
+DownloadEntriesVid(r'C:\DSX_Vis\export\2023-05-24_FullClean.json', originalBudget=10000,
+                   levelName='Sandbox')
---
+++ scripts/levelsummary.py
@@ -0,0 +1,5 @@
+# accessing script with functions
+from gallerydata import *
+
+# filter and export clean data based on levelName
+AllEntriesForLevel(r'C:\DSX_Vis\export\2023-05-24_FullClean.json', 'Sandbox')

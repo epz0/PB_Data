ea8d6296218058ece85979ff18d1e30d95d4f513
eparavizo <ep650@cam.ac.uk>
2024-01-28 10:41:03 +0000

    chore: path update

--- scripts/downloadvideos.py
+++ scripts/downloadvideos.py
@@ -5,5 +5,5 @@ from gallerydata import *
 # originalBudget is the level budget
 # levelName is the name of the level on the API
 # these info can be retrieved from \support\LevelsInfo-master.xlsx
-DownloadEntriesVid(r'C:\DSX_Vis\export\2023-05-24_FullClean.json', originalBudget=10000,
+DownloadEntriesVid(r'C:/Py/DSX_Vis/export/2023-05-24_FullClean.json', originalBudget=10000,
                    levelName='Sandbox')
